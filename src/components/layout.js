5badf362b7fd6b6af5650f400e4944f59a0c4363
acccdang <acccdang8659@gmail.com>
2021-09-25 22:54:01 +0900

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "gatsby";
+import { container, heading, navLinks, navLinkText } from "./layout.module.css";
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li class>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>About 화면입니다.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
